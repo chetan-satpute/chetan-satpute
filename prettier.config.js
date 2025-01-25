fc703fed8b4cecf03e4a6fa0c237f97279d3ab6e
Chetan Satpute <chetan.satpute2002@gmail.com>
2025-01-25 11:12:23 +0530

    project setup

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+export default {
+  tabWidth: 2,
+  singleQuote: true,
+  bracketSpacing: false,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="from-primary flex h-screen w-screen items-center justify-center bg-linear-to-t to-white">
+      <h1 className="text-2xl font-bold text-neutral-200">Chetan Satpute</h1>
+    </div>
+  );
+}
+
+export default App;
