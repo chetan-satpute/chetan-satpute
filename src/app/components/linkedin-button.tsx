edb34e13df5adfcdc2f14dad3a8421546f9af963
Chetan Satpute <chetan.satpute2002@gmail.com>
2025-01-26 08:53:05 +0530

    add robots.txt and alt for images

--- src/app/components/linkedin-button.tsx
+++ src/app/components/linkedin-button.tsx
@@ -3,7 +3,7 @@ import linkedinLogo from '../../assets/images/In-Blue-26.png';
 function LinkedinButton() {
   return (
     <span className="flex gap-2">
-      <img src={linkedinLogo} className="h-6 w-6" />
+      <img src={linkedinLogo} className="h-6 w-6" alt="linkedin logo" />
       <a
         href="https://www.linkedin.com/in/chetansatpute"
         target="_blank"
