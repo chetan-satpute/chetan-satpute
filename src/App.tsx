a5d791d2b0e363d18efb5ae1f8df9a40a6a6ca4e
Chetan Satpute <chetan.satpute2002@gmail.com>
2026-01-03 12:02:38 +0530

    chore: setup project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1 className="text-xl font-bold">Chetan Satpute</h1>
+    </div>
+  );
+}
+
+export default App;
